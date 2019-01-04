c976fc7da3d9e67800e0b087e368137a5ad16212
shotaogasawara <35189691+shotaogasawara@users.noreply.github.com>
2019-01-04 16:58:11 +0900

    refactor: アロー関数を使って書き換え

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,10 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder'
 
-export const fetchPosts = () => {
-  return async function(dispatch, getState) {
-    const response = await jsonPlaceholder.get('/posts')
+export const fetchPosts = () => async dispatch => {
+  const response = await jsonPlaceholder.get('/posts')
 
-    dispatch({
-      type: 'FETCH_POSTS',
-      payload: response,
-    })
-  }
+  dispatch({
+    type: 'FETCH_POSTS',
+    payload: response,
+  })
 };
